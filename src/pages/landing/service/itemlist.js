701f99fea3e466fce8834ef666ac64051956b346
zeus-dev919 <cccodechef@gmail.com>
2022-09-30 04:39:59 +0900

    service description changed

--- src/pages/landing/service/itemlist.js
+++ src/pages/landing/service/itemlist.js
@@ -3,22 +3,22 @@ export const itemList = [
       imagename: "nft",
       imageurl: require("../../../assets/images/service_nft.png"),
       title: "NFT Insurance",
-      content: "Pixpel provides insurance for the players’ NFT up to 70% of the purchase value for 3 months, with the option to be renewed every 3 months for up to 3 renewals.Games with insurance will be easily recognized with a check mark."
+      content: "In NFT insurance Change o tis text please: Pixpel provides a unique NFT insurance policy, worth up to 70% of the NFT purchase value. Our insurance lasts for 3 months, with the option to be renewed up to 3 times. Games with insurance have a checkmark, making them easily recognizable."
     },
     {
         imagename: "marketplace",
         imageurl: require("../../../assets/images/service_marketplace.png"),
         title: "Game & NFT Marketplace",
-        content: "Pixpel combines an NFT marketplace and game marketplace, where players can check the offers made by other players, including sell history, or newly minted NFTs by game developers. Developers will show the game in the right way, with visuals, game descriptions, and logistics."
+        content: "Pixpel combines an NFT marketplace and a game marketplace, where players can check offers made by other players, sell history, or newly minted NFTs by game developers. Developers have the opportunity to fully present their NFTs and games with visuals, game descriptions, and logistics."
     },{
         imagename: "wallet",
         imageurl: require("../../../assets/images/service_wallet.png"),
         title: "Game Wallet",
-        content: "Our specialized gaming wallet allows players to get the most out of their NFTs from games. To increase the efficiency of gaming and trading, we segmented our wallet into different accounts. Pixpel wallet also includes NFT in-game traceability. "
+        content: "Our specialized gaming wallet allows players to get the most out of their NFTs and games. To increase the efficiency of gaming and trading, we segmented our wallet into different, specialized accounts. Pixpel wallet also includes NFT in-game traceability."
     },{
         imagename: "decentralization",
         imageurl: require("../../../assets/images/service_decentralization.png"),
-        title: "Centralized Exchange",
-        content: "Pixpel offers an exchange service with the lowest fee in the market. Games listed in Pixpel will have the chance to list their tokens to get liquidity in the early stages of the games. "
+        title: "DEX & CEX",
+        content: "Pixpel offers exchanges service with the most competitive prices on the market. Games listed in Pixpel will have the chance to list their tokens to get liquidity during their early stages."
     },
 ]
