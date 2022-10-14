775187a5d27894e8aed8be380419d7dfa7333912
zeus-dev919 <cccodechef@gmail.com>
2022-10-15 03:12:18 +0900

    landing page complete

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -55,10 +55,10 @@ function Header() {
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0">
             <li>
               <button className="
-                flex justify-between items-center text-sm2 w-full text-white hover:text-app-green"
+                flex justify-between items-center text-sm2 w-full text-app-green"
                 onClick={() => { setNavbarOpen(!navbarOpen); }}
               >
-                More 
+                Menu 
                 <svg className="mt-1 ml-1 w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd">
                   </path>
--- src/pages/landing/release/index.tsx
+++ src/pages/landing/release/index.tsx
@@ -4,9 +4,9 @@ import tcu from "../../../assets/images/TCU.png";
 
 function Release() {
   return (
-    <div className="flex flex-col gradientbackground-release w-full py-16 items-center">
+    <div className="flex flex-col gradientbackground-release w-full py-20 items-center">
       <div className="text-3xl font-semibold mb-8 text-white">Press Release</div>
-      <div className="flex flex-col lg:flex-row lg:gap-24 gap-8 items-center hr-gradient">
+      <div className="flex flex-col lg:flex-row lg:gap-24 gap-8 items-center hr-gradient mb-10  ">
         <img src={yahoo} alt="yahoo" />
         <div className="text-3xl font-bold text-white">europa press</div>
         <img src={tcu} alt="tcu" />
